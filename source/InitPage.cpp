704d4d7788b79407add71731ffb78b3d20ea731b
LeeSooBin96 <a9622880@naver.com>
2024-01-20 00:14:33 +0900

    step2_2

--- source/InitPage.cpp
+++ source/InitPage.cpp
@@ -1,11 +1,15 @@
+#include <cstdlib>
 #include <iostream>
 
+#include "Database.h"
 #include "DetailPage.h"
 #include "GetCh.h"
 #include "InitPage.h"
 #include "SearchPage.h"
 
 using std::cout;
+
+sqlite3* db;
 
 //공고 목록 출력 함수 - 커서 위치 입력받아서 색 변경 적용
 void InitPage::ShowNotice(unsigned short pos)
@@ -48,8 +52,11 @@ void InitPage::ListforPos(unsigned short pos)
 //초기 페이지 메인 프로세스
 void InitPage::ProcessInit(void)
 {
+    srand(time(NULL));
     while(true)
     {    
+        //일단 다른거 생각안하고 공고 랜덤으로 3~5개 출력되도록
+        unsigned short lnumMax = rand()%3+3; //목록 개수
         //구성: 공고 목록/ 검색 입력/ 로그아웃
         unsigned short pos=0;
         bool quit=false;
@@ -68,7 +75,7 @@ void InitPage::ProcessInit(void)
                     ListforPos(pos);
                     break;
                 case 66: //방향키 하
-                    if(pos!=2) pos++;
+                    if(pos!=2+lnumMax) pos++;
                     system("clear");
                     ListforPos(pos);
                     break;
@@ -79,29 +86,44 @@ void InitPage::ProcessInit(void)
                     break;
             }
         }
-        switch (pos)
+        if(pos==0) //로그아웃
         {
-        case 0: //로그아웃
             cout<<"로그아웃을 진행합니다. \n";
             return;
-        case 1: //공고 상세 정보
+        }
+        else if(pos>0&&pos<lnumMax+1) //공고 상세 정보
         {
-            cout<<"공고의 상세 정보를 조회합니다. \n";
-            //공고 상세 정보 페이지
             DetailPage detail;
             detail.ProcessDetail();
-            break;
         }
-        case 2: //검색
+        else if(pos==lnumMax+1) //검색
         {
-            cout<<"검색 결과를 출력합니다. \n";
-            //검색 페이지
             SearchPage search;
             search.ProcessSearch();
-            break;
         }
-        default:
-            break;
-        }
+        // switch (pos)
+        // {
+        // case 0: //로그아웃
+        //     cout<<"로그아웃을 진행합니다. \n";
+        //     return;
+        // case 1: //공고 상세 정보
+        // {
+        //     cout<<"공고의 상세 정보를 조회합니다. \n";
+        //     //공고 상세 정보 페이지
+        //     DetailPage detail;
+        //     detail.ProcessDetail();
+        //     break;
+        // }
+        // case 2: //검색
+        // {
+        //     cout<<"검색 결과를 출력합니다. \n";
+        //     //검색 페이지
+        //     SearchPage search;
+        //     search.ProcessSearch();
+        //     break;
+        // }
+        // default:
+        //     break;
+        // }
     }
 }
